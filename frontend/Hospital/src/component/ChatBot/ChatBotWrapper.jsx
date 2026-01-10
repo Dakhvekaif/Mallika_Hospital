c33541efde4e52d059e55350c4a8eaa35a8c19a9
HuzaifaChouhan <huzefachouhan07@gmail.com>
2026-01-10 17:51:25 +0530

    chatbot updated

--- frontend/Hospital/src/component/ChatBot/ChatBotWrapper.jsx
+++ frontend/Hospital/src/component/ChatBot/ChatBotWrapper.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import HospitalChatbot from "./HospitalChatBot";
+import Botimg from '../../assets/ChatBot/Goldy.png';
 
 export default function ChatbotWrapper() {
     const [open, setOpen] = useState(false);
@@ -7,15 +8,22 @@ export default function ChatbotWrapper() {
     return (
         <>
             <button
+              type="button"
               onClick={() => setOpen(true)}
-              className="fixed bottom-6 right-6 z-40
+              className="fixed bottom-0 left-0 z-40
                 bg-blue-600 text-white px-5 py-3 rounded-full shadow-xl
-                flex items-center gap-2 text-base
-                hover:scale-105 transition"
+                flex items-center gap-2 scale-80 md:scale-100 bottom-6 left-4
+                hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-400
+                transition"
+              aria-label="Chat with Umendra"
             >
-              💬 Chat with Umendra
+              <img
+                src={Botimg}
+                alt="Chat bot"
+                className="w-10 h-10"
+              />
+              <span>Chat with Mallika</span>
             </button>
-
 
             <HospitalChatbot open={open} setOpen={setOpen} />
         </>
